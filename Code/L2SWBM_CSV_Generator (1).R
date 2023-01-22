dbec3e57dc2e541d7111f4f5e6fb0aac5aa31730
manishrv <manishrv@umich.edu>
2023-01-21 22:53:17 -0500

    Updated Code

--- Code/L2SWBM_CSV_Generator (1).R
+++ Code/L2SWBM_CSV_Generator (1).R
@@ -4,8 +4,8 @@ library(openxlsx)
 
 #Set the folder where the spreadsheet is in the setwd() function,
 #or move the spreadsheet to the same folder the code is in and comment the line
-
-setwd("C://Users//manis//OneDrive - Umich//Documents//PersonalMonoLakePlayground//DataOrganization//Spreadsheets")
+wd = getwd()
+setwd(wd)
 
 
 #Function to remove zeroes -> Not Important
@@ -31,20 +31,28 @@ print(indexes)
 
 
 #Read the Data Sheet
-raw_data = data.frame(read_excel("Mono_Lake_All_Data_Spreadsheet.xlsx",na=c("NA","")))
+raw_data = data.frame(read_excel("G:\\Shared drives\\SEAS-Hydro (MONO LAKE)\\Mono_Lake_All_Data_Spreadsheet.xlsx",na=c("NA","")))
 colnames = colnames(raw_data)
-header_rows = 5
+header_rows = 6
 data_rows = c(header_rows+1:1546)
 
 #Select Columns for L2SWBM
-in_situ_products_all = c(17:36)
-in_situ_overlake_product = c(17,27, 23,33,22,24,26,29,30,28,36)  #Stations in OverLake Product: 040943 (Bodie), Cain, 040684, MNL, 044881, 045779, UNDC1, CAMN1 (CAMN001?) (only 11 months with discrepancies), CAMN6, CQ251, C4643
-global_precip = c(5:10)
-global_evap = c(11:16)
-diversions = 40 #Only one currently to be used is East Portal IIRC
-water_level = 4
-natural_streamflow = c(41)
-actual_streamflow = c()
+in_situ_products_all = which(TRUE==grepl("ipa|ip",raw_data[3,]))
+in_situ_overlake_product = which(raw_data[3,]=="ipa")  #Stations in OverLake Product: 040943 (Bodie), Cain, 040684, MNL, 044881, 045779, UNDC1, CAMN1 (CAMN001?) (only 11 months with discrepancies), CAMN6, CQ251, C4643
+global_precip = which(raw_data[3,]=="p")
+evap = which(raw_data[3,]=="e")
+water_level = which(raw_data[1,]=="Mono_Lake_Water_Levels")
+TNC_rush = which(raw_data[1,]=="TNC - Rush" )
+TNC_LV = which(raw_data[1,]=="TNC - Lee Vining" )
+MTWP = which(raw_data[1,]=="MTWP - MONO TUNNEL AT WEST PORTAL" )
+GLSC = which(raw_data[1,]=="GLRSC - GRANT LAKE RESERVOIR STORAGE CHANGE" )
+DWP_LV = which(raw_data[1,]=="5009 - LEE VINING CREEK AT INTAKE" )
+DWP_WC = which(raw_data[1,]=="5002 - WALKER CREEK UNDER CONDUIT" )
+DWP_PC = which(raw_data[1,]=="5003 - PARKER CREEK UNDER CONDUIT" )
+GLRML = which(raw_data[1,]=="GLRML - GRANT LAKE RELEASE TO MONO LAKE" )
+water_volume = which(raw_data[3,]=="v")
+
+
 
 #Create L2SWBM Spreadsheet
 l2swbm_data = data.frame(date = seq(from = as.Date("1895-01-01"), to = as.Date("2023-10-01"), by = 'month'))
@@ -54,8 +62,7 @@ runoff_counter=1
 temp_work =data.frame(date = raw_data$Date[data_rows])
 
 #####DStore#####
-l2swbm_data$yd1.DStore_StaffGage = as.numeric(unlist(raw_data[data_rows,water_level]))*as.numeric(unlist(data_rows,raw_data$Area))
-
+l2swbm_data$yd1.Store_StaffGage = raw_data[data_rows,water_volume]
 #####Averages of Insitu############
 
 counter=2
@@ -78,7 +85,7 @@ for (i in global_precip){ #Get Volumes of each product & Create Column Names
   precip_counter=precip_counter+1
 }
 
-for (i in global_evap){ #Get Volumes of each product & Create Column Names
+for (i in evap){ #Get Volumes of each product & Create Column Names
   raw_data[data_rows,i] = (as.numeric((raw_data[data_rows,i]))/12)*as.numeric(raw_data$Area[data_rows])
   coln = paste("ye",toString(evap_counter),".",strsplit(colnames[i], split = "[.]")[[1]][1], sep = "")
   l2swbm_data$temp = as.numeric(raw_data[data_rows,i])
@@ -88,16 +95,20 @@ for (i in global_evap){ #Get Volumes of each product & Create Column Names
 
 #####Runoff Calculation######
 
-for (i in natural_streamflow){
-  raw_data[data_rows,i] = (as.numeric(raw_data[data_rows,i])*0.000810714)-as.numeric(raw_data[data_rows,diversions]) #Cubic Meters to Acre Feet
-  coln = paste("yr",toString(runoff_counter),".",strsplit(colnames[i], split = "[.]")[[1]][1], sep = "")
-  l2swbm_data$temp = as.numeric(raw_data[data_rows,i])
-  colnames(l2swbm_data)[which(names(l2swbm_data) == "temp")] <- coln
-  runoff_counter=runoff_counter+1
-}
+l2swbm_data$DWP_act = as.numeric(raw_data[data_rows,DWP_PC])+as.numeric(raw_data[data_rows,DWP_WC])+as.numeric(raw_data[data_rows,DWP_LV])+as.numeric(raw_data[data_rows,GLRML])
+l2swbm_data$TNC_act = as.numeric(raw_data[data_rows,TNC_rush])+as.numeric(raw_data[data_rows,TNC_LV])-as.numeric(raw_data[data_rows,MTWP])-as.numeric((raw_data[data_rows,GLSC]))
+  
+
+#for (i in natural_streamflow){
+#  raw_data[data_rows,i] = (as.numeric(raw_data[data_rows,i])*0.000810714)-as.numeric(raw_data[data_rows,diversions]) #Cubic Meters to Acre Feet
+#  coln = paste("yr",toString(runoff_counter),".",strsplit(colnames[i], split = "[.]")[[1]][1], sep = "")
+#   l2swbm_data$temp = as.numeric(raw_data[data_rows,i])
+#   colnames(l2swbm_data)[which(names(l2swbm_data) == "temp")] <- coln
+#   runoff_counter=runoff_counter+1
+# }
 
 #Use this function to remove zeroes in data with averages of surronding values
 #removezeroes(as.Date("1905-01-01"),as.Date("2005-01-01"))
 
-write.csv(l2swbm_data, "L2SWBM_data.csv", row.names=FALSE, na = "")
+#write.csv(l2swbm_data, "L2SWBM_data.csv", row.names=FALSE, na = "")
 
--- Code/Precip_Rate_Yearly.R
+++ Code/Precip_Rate_Yearly.R
@@ -3,15 +3,21 @@ library(dplyr)
 library(plyr)
 ### Mono Lake Stage-Storage-Area-Salinity Curves ###
 wdir = paste("C:/Users/manis/OneDrive - Umich/Documents/PersonalMonoLakePlayground/DataOrganization")
-setwd(wdir)
+indir = paste("C:/Users/manis/OneDrive - Umich/Documents/PersonalMonoLakePlayground/DataOrganization")
+outdir = paste("C:/Users/manis/OneDrive - Umich/Documents/PersonalMonoLakePlayground/DataOrganization/Plots")
+setwd(indir)
 ## Takes a y vector, returns start and end points of data and number of segments
 
 
-y = data.frame(read.csv("Spreadsheets//L2SWBM_data.csv",na.strings=""))
-y[,1] = as.Date(as.character(y[,1]), tryFormats=c("%Y-%m-%d"))
+p = data.frame(read.csv("Spreadsheets//Precipitation_global.csv",na.strings=""))
+p$CAPA = as.numeric(p$CAPA)
+p[,1] = as.Date(as.character(p[,1]), tryFormats=c("%m/%d/%Y"))
+e = data.frame(read.csv("Spreadsheets//Evaporation_PET.csv"))
+e[,1] = as.Date(as.character(e[,1]), tryFormats=c("%m/%d/%Y"))
+y <- merge(x=p,y=e, 
+           by="Date", all=TRUE)
 
-
-y$years <- format(y$date, "%Y")
+y$years <- format(y$Date, "%Y")
 x_years <- as.numeric(unique(y$years))
 startyear = strtoi(y$years[1])
 endyear = strtoi(y$years[length(y$years)])
@@ -21,7 +27,7 @@ endyear = strtoi(y$years[length(y$years)])
 
 
 
-plotname=paste("Plots//Precip_Evap_Global_Yearly.pdf") #Comment to SIP3
+plotname=paste(outdir,"Precip_Evap_Global_Yearly.pdf") #Comment to SIP
 pdf(file=plotname, height = 6, width = 9, paper = "special") 
 par(xpd=TRUE)
 par(mfrow=c(2,1))
@@ -29,18 +35,21 @@ par(mar = c(0, 4, 0, 0))
 par(oma = c(3,4,1,3))
 
 
-
-s_m = 1300
-e_m = s_m+120
+#y[,1] = as.Date(as.character(y[,1]), tryFormats=c("%Y-%m-%d"))
+#precip_cols = c(2:7)
+#y$avg_in_situ = rowMeans(y[precip_cols], na.rm=TRUE)
+#s_m = 1300
+#e_m = s_m+120
 startMo = y$Date[s_m]
 endMo = y$Date[e_m]
 xpol = c(startMo, startMo,endMo,endMo)
 cols <- brewer.pal(8,'Dark2')
 
 ## Precip_global_sample##
-plot(c(0),c(0),xlab="Date", ylab="Plots//Precip Rate (Acre Feet)", type = "n", col = 8, lwd = 3, axes = FALSE,ylim=c(0,250000), xlim = c(1940,2023))
+plot(c(0),c(0),xlab="Date", ylab="Precip Rate (in)", type = "n", col = 8, lwd = 3, axes = FALSE,
+     ylim = c(0,75)*1.1, xlim = c(1901,2022))
 legend=array(NA)
-precip_cols = which(grepl("yp",colnames(y))==TRUE)
+precip_cols = c(2:7)
 legend_counter=1
 for (i in precip_cols) {
   print(i)
@@ -51,34 +60,31 @@ for (i in precip_cols) {
   legend_counter=legend_counter+1
 }
 axis(2, labels=T, cex.axis = 0.9)
-axis(3, x_years, cex=0.9, labels=F)
+axis(3, pretty(x_years), cex=0.9, labels=F)
 box()
 legend("topleft", legend = legend, lty=c(1), 
-       col=cols, cex=0.8,bty="n")
+       col=cols, cex=0.8)
 
 ##Evaporation_global_products##
-plot(c(0),c(0),xlab="Date", ylab="Evap Rate (Acre Feet)", type = "n", col = 8, lwd = 3, axes = FALSE,
-     ,ylim=c(0,250000), xlim = c(1940,2023))
+plot(c(0),c(0),xlab="Date", ylab="Evap Rate (in)", type = "n", col = 8, lwd = 3, axes = FALSE,
+     ylim = c(0,75)*1.1, xlim = c(1901,2022))
 legend=array(NA)
-precip_cols = which(grepl("ye",colnames(y))==TRUE)
+precip_cols = c(8:13)
 
 legend_counter=1
 for (i in precip_cols) {
   print(i)
-  temp = tapply(as.numeric(y[,i]),y$years,sum)
-  s = as.numeric(data.frame(template=names(temp),sum=temp)$sum)
+  temp = tapply(y[,i],y$years,sum)
+  s = data.frame(template=names(temp),sum=temp)$sum
   lines(x_years, s, col = cols[legend_counter],  lwd = 1)
   legend[legend_counter]=colnames(y)[i]
   legend_counter=legend_counter+1
 }
 axis(4, labels=T, cex.axis = 0.9)
-axis(1, x_years, cex=0.9, labels=T)
+axis(1, pretty(x_years), cex=0.9, labels=T)
 box()
 legend("topleft", legend = legend, lty=c(1), 
-       col=cols, cex=0.8,bty="n")
+       col=cols, cex=0.8)
 
 
-
-#dev.off()
-
-
+dev.off()
